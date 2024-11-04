8353b7630d5d74c961f07848d0498c1a591c9bc4
alif2499 <alifjcc99@gmail.com>
2024-11-04 12:37:57 -0700

    Modifying notebooks after file organization

--- data_files/link_opening.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var links = document.querySelectorAll('a');
-    links.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent default link behavior
-            window.location.href = link.href; // Open link in the same tab
-        });
-    });
-});
